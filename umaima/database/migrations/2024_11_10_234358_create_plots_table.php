2abf5bd8ec954c3e40771bb6d486c581e465bf27
030439 <praimchand763@gmail.com>
2024-11-11 04:44:33 +0500

    created plot migration

---
+++ umaima/database/migrations/2024_11_10_234358_create_plots_table.php
@@ -0,0 +1,25 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlotsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('plots', function (Blueprint $table) {
+            $table->id();
+            $table->string('plot_number')->unique();
+            $table->foreignId('scheme_id')->constrained('schemes')->onDelete('cascade');
+            $table->foreignId('plot_size_id')->constrained('plot_sizes')->onDelete('cascade');
+            $table->foreignId('plot_location_id')->constrained('plot_locations')->onDelete('cascade');
+            $table->foreignId('plot_category_id')->constrained('plot_categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('plots');
+    }
+}
