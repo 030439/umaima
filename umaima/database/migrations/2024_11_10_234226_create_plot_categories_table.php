e069b6dac8e6c31e13d7b44f48eef5c7836a9a94
030439 <praimchand763@gmail.com>
2024-11-11 04:42:59 +0500

    created plot cateogry

---
+++ umaima/database/migrations/2024_11_10_234226_create_plot_categories_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlotCategoriesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('plot_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category_name')->unique();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('plot_categories');
+    }
+}
