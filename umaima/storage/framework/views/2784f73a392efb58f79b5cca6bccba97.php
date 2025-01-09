592ac903f6354ac6a4a60961abae2c99a184e3cc
030439 <praimchand763@gmail.com>
2025-01-09 23:22:18 +0500

    payment store duplication

---
+++ umaima/storage/framework/views/2784f73a392efb58f79b5cca6bccba97.php
@@ -0,0 +1,102 @@
+
+<?php $breadCrumb='Expenses / <span class="text-primary">Expense Heads</span></i>'?>
+<?php $__env->startSection('title', 'Expense-head'); ?>
+<?php $__env->startSection('content'); ?>
+   
+
+
+<!-- / Navbar -->
+
+      
+
+      <!-- Content wrapper -->
+      <div class="content-wrapper">
+
+        <!-- Content -->
+        
+          <div class="container-xxl flex-grow-1 container-p-y">
+            
+            
+
+<!-- Product List Table -->
+<div class="card">
+  <div class="card-header">
+    <h5 class="card-title">Filter</h5>
+    <div class="d-flex justify-content-between align-items-center row pt-4 gap-6 gap-md-0">
+        <div class="col-md-4">
+            <select id="paymentType" class="form-select">
+                <option value="">Filter By Payement Type</option>
+                <option value="1">Received </option>
+                <option value="2">Payment</option>
+            </select>
+        </div>
+        <div class="col-md-4">
+            <select id="subcat" class="form-select text-capitalize">
+            </select>
+        </div>
+        <div class="col-md-4">
+            <input type="text" class="form-control flatpickr-input" placeholder="YYYY-MM-DD to YYYY-MM-DD" id="flatpickr-range" readonly="readonly">
+        </div>
+    </div>
+  </div>
+  <div class="card-datatable table-responsive">
+    <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper dt-bootstrap5 no-footer"><div class="card-header d-flex border-top rounded-0 flex-wrap py-0 flex-column flex-md-row align-items-start"><div class="me-5 ms-n4 pe-5 mb-n6 mb-md-0">
+</div>
+        <div class="d-flex justify-content-start justify-content-md-end align-items-baseline">
+          </div></div></div>
+            <table class="datatables-products table dataTable no-footer dtr-column collapsed" id="DataTables_Table_0" aria-describedby="DataTables_Table_0_info" style="width: 958px;">
+                
+            </table>
+        </div>
+  </div>
+</div>
+
+
+          </div>
+          <!-- / Content -->
+
+          <?php $__env->stopSection(); ?> 
+  
+
+    
+<?php $__env->startSection('files'); ?>
+
+<!-- Core JS -->
+<!-- build:js assets/vendor/js/core.js -->
+
+<script src="../../assets/vendor/libs/jquery/jquery.js"></script>
+<script src="../../assets/vendor/libs/popper/popper.js"></script>
+<script src="../../assets/vendor/js/bootstrap.js"></script>
+  <script src="../../assets/vendor/libs/node-waves/node-waves.js"></script>
+<script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="../../assets/vendor/libs/hammer/hammer.js"></script>
+<script src="../../assets/vendor/libs/i18n/i18n.js"></script>
+<script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
+<script src="../../assets/vendor/js/menu.js"></script>
+
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+<script src="../../assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
+
+<!-- Main JS -->
+<script src="../../assets/vendor/libs/moment/moment.js"></script>
+<script src="../../assets/vendor/libs/flatpickr/flatpickr.js"></script> 
+<script src="../../assets/vendor/libs/bootstrap-datepicker/bootstrap-datepicker.js"></script>
+<script src="../../assets/vendor/libs/bootstrap-daterangepicker/bootstrap-daterangepicker.js"></script>
+<script src="../../assets/vendor/libs/jquery-timepicker/jquery-timepicker.js"></script>
+<script src="../../assets/vendor/libs/pickr/pickr.js"></script>
+
+<!-- Main JS -->
+<script src="../../assets/js/main.js"></script>
+
+
+<!-- Page JS -->
+<script src="../../assets/js/forms-pickers.js"></script>
+
+<!-- Page JS -->
+<script src="../../assets/js/app-ecommerce-product-list.js"></script>
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layout.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\umaima\umaima\resources\views/accounts/index.blade.php ENDPATH**/ ?>
