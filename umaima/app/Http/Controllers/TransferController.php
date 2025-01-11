a9c5f36640b78987107f395e0855bf6503a60411
030439 <praimchand763@gmail.com>
2025-01-11 18:34:26 +0500

    plot services modification for transfer of plot

--- umaima/app/Http/Controllers/TransferController.php
+++ umaima/app/Http/Controllers/TransferController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\PlotService;
 
 class TransferController extends Controller
 {
-    //
+
+    protected $plotservice;
+    
+    public function __construct(PlotService $plotservice)
+    {
+        $this->plotservice = $plotservice;
+    }
+
+    public function plotTransfer(){
+        return view('transfer.plots');
+    }
 }
